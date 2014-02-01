65218cec3f49cf7a9af008a0b9527fb318b0b429
edomaru <masaruedo@yahoo.com>
2014-02-02 05:27:57 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearRailsFirstApp::Application.config.secret_key_base = 'd32201bf5b6677cf075d3db59c395f01ca5e9c86056380aafa57fd422d93906aa0ea9e76dafb8f94d5eea3e2e212884da0f5e41ec9a1d3f147114756426058bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearRailsFirstApp::Application.config.session_store :cookie_store, key: '_lear_rails_first_app_session'
